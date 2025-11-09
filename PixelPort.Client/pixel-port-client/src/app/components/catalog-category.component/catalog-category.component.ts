e970075896d726da0737bd784ed310c4b374376c
ne4eHbka58 <artem03082006@mail.ru>
2025-11-09 18:59:10 +0300

    Добавлен переход со страницы категорий к каталогу и применение соответствующего фильтра. На страницу категорий добавлены новые изображения

--- PixelPort.Client/pixel-port-client/src/app/components/catalog-category.component/catalog-category.component.ts
+++ PixelPort.Client/pixel-port-client/src/app/components/catalog-category.component/catalog-category.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { FilterSortService } from '../../data/services/filter-sort.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-catalog-category',
@@ -11,4 +13,17 @@ export class CatalogCategory {
   // Входные данные
   @Input() title: string = 'Телефоны'; // Название категории
   @Input() imageSrc: string = '/assets/images/phone.png'; // Ссылка на изображение
+  @Input() categoryId: number = 1; // Номер категории, по которой будет фильтрация
+
+  // Сервисы
+  private filterSortService = inject(FilterSortService);
+  private router = inject(Router);
+
+  onCategoryClick(): void {
+    // Устанавливаем фильтр категории
+    this.filterSortService.setCategoryFilter(this.categoryId);
+
+    // Переходим на страницу каталога
+    this.router.navigate(['/catalog']);
+  }
 }
